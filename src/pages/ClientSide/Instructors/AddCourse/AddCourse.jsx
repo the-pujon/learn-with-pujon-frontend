5411f560aa69c499a35fe6431bdd8d07207cc8fb
Pujon Das Auvi <auvipujondas@gmail.com>
2024-01-13 04:14:31 +0600

    add course updated

--- src/pages/ClientSide/Instructors/AddCourse/AddCourse.jsx
+++ src/pages/ClientSide/Instructors/AddCourse/AddCourse.jsx
@@ -121,8 +121,8 @@ const AddCourse=() => {
 
   return (
     <div className="">
-      <div className="flex wrapper min-h-screen pt-16 items-center justify-center gap-4">
-        <div className="flex-1  flex items-center justify-center ">
+      <div className="flex flex-col sm:flex-row wrapper min-h-screen pt-16 items-center justify-center">
+        <div className="flex-1 pt-5 sm:pt-0 flex items-center justify-center ">
           {upLoadedImages? (
             <img
               src={URL.createObjectURL(upLoadedImages)}
@@ -130,9 +130,9 @@ const AddCourse=() => {
               className="w-full rounded-3xl"
             />
           ):(
-            <div className="border border-primary w-80  h-80 rounded-full flex items-center justify-center">
+            <div className="border border-primary w-44 sm:w-80 h-44 sm:h-80 rounded-full flex items-center justify-center">
               {" "}
-              <img src={upImage} alt="" className="  p-5 w-52" />
+              <img src={upImage} alt="" className="  p-5 w-32 sm:w-52" />
             </div>
           )}
         </div>
