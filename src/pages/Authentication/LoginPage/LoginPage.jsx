6be33223f2fd8f7a9478953964ad0be8c89d9365
Pujon Das Auvi <auvipujondas@gmail.com>
2024-01-12 20:56:44 +0600

    sign up page complete

--- src/pages/Authentication/LoginPage/LoginPage.jsx
+++ src/pages/Authentication/LoginPage/LoginPage.jsx
@@ -1,22 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
 import logo1 from "../../../assets/logos/logo1.png";
-import {FaGoogle} from "react-icons/fa";
-import {Link} from "react-router-dom";
+import { FaGoogle } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { useUser } from "../../../Hooks/useUser";
+import { toast } from "react-hot-toast";
+import {  useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
+  const { loginWithEmail, loginWithGoogle, loginWithGithub } = useUser();
+
+  const [error, setError] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+
+    const email = form.email.value;
+    const password = form.password.value;
+
+    //login with email
+    loginWithEmail(email, password)
+      .then((result) => {
+        toast.success("Login Successful", { position: "top-right" });
+        navigate("/");
+      })
+      .catch((err) => {
+        console.error(err);
+        toast.error("Wrong email or password", { position: "top-right" });
+        setError(true);
+      });
+  };
+
+  const handleGoogleLogin = () => {
+    console.log("sd");
+    loginWithGoogle()
+      .then((res) => {
+        const { displayName, email, photoURL } = res.user;
+
+        fetch(`https://sv-ashen.vercel.app/api/users/${email}`)
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data) {
+              navigate("/");
+            } else {
+              fetch("https://sv-ashen.vercel.app/api/users", {
+                method: "POST",
+                headers: { "Content-type": "application/json" },
+                body: JSON.stringify({
+                  name: displayName,
+                  email: email,
+                  image: photoURL,
+                }),
+              })
+                .then((res) => res.json())
+                .then((data) => {
+                  console.log(data);
+                  navigate("/");
+                })
+                .catch((err) => console.error(err));
+            }
+          });
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="w-1/4 p-10 flex items-center justify-center flex-col rounded-md shadow-2xl"
-      style={{
-        background: 'conic-gradient(from 270deg at 0% 0%, rgba(255, 255, 255, .5) 50%, rgba(33, 53, 85, 1) 100%)'
-      }}
+      <div
+        className="w-1/4 p-10 flex items-center justify-center flex-col rounded-md shadow-2xl"
+        style={{
+          background:
+            "conic-gradient(from 270deg at 0% 0%, rgba(255, 255, 255, .5) 50%, rgba(33, 53, 85, 1) 100%)",
+        }}
       >
-
         <div>
           <img src={logo1} alt="" className="w-32" />
         </div>
-        <div className="text-4xl mt-4 text-primary font-bold" >Login</div>
-        <form action="" className="w-full flex items-center gap-5 mt-10 flex-col" >
+        <div className="text-4xl mt-4 text-primary font-bold">Login</div>
+        <form
+          action=""
+          className="w-full flex items-center gap-5 mt-10 flex-col"
+          onSubmit={handleLogin}
+        >
           <div className="form-control relative  w-full">
             <input
               autoComplete="off"
@@ -52,18 +121,22 @@ const LoginPage = () => {
             </label>
           </div>
 
-          <button className="SVButton-2 py-2 px-3 mt-3 w-full text-primary" >
+          <button className="SVButton-2 py-2 px-3 mt-3 w-full text-primary">
             <span>Login</span>
           </button>
         </form>
-        <div className="divider divide-white divide-x-2 divide-y-2 text-primary">OR</div>
+        <div className="divider divide-white divide-x-2 divide-y-2 text-primary">
+          OR
+        </div>
         <div>
-            <button className="border p-2 rounded-full border-primary" >
-                <FaGoogle className="text-5xl text-primary"/>
-            </button>
+          <button onClick={handleGoogleLogin} className="border p-2 rounded-full border-primary">
+            <FaGoogle className="text-5xl text-primary" />
+          </button>
         </div>
 
-        <div className="text-primary mt-4" >Do not have an account? <Link to={'/signup'}>Sign up</Link></div>
+        <div className="text-primary mt-4">
+          Do not have an account? <Link to={"/signup"}>Sign up</Link>
+        </div>
       </div>
     </div>
   );
--- src/pages/Authentication/SignUpPage/SignUpPage.jsx
+++ src/pages/Authentication/SignUpPage/SignUpPage.jsx
@@ -1,87 +1,215 @@
-import React from 'react'
+import React from "react";
 import logo1 from "../../../assets/logos/logo1.png";
-import {FaGoogle} from "react-icons/fa";
-import {Link} from "react-router-dom";
-
+import { FaGoogle } from "react-icons/fa";
+import { useUser } from "../../../Hooks/useUser";
+import { useNavigate, Link } from "react-router-dom";
+import { auth } from "../../../Firebase/firebase.config";
+import { updateProfile } from "firebase/auth";
+import useApi from "../../../Hooks/useApi";
+import { toast } from "react-hot-toast";
 const SignUpPage = () => {
+  const { registrationWithEmail, loginWithGoogle } = useUser();
+  const { get, post } = useApi();
+
+  const navigate = useNavigate();
+
+  //registration by email
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+
+    const name = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    const photoURL = form[3].files[0];
+
+    const apiKey = import.meta.env.VITE_IMGBB_API_KEY;
+    const apiUrl = import.meta.env.VITE_IMGBB_API_URL;
+
+    const formData = new FormData();
+    formData.append("image", photoURL);
+
+    fetch(`${apiUrl}?key=${apiKey}`, {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Failed to upload image");
+        }
+      })
+      .then((data) => {
+        registrationWithEmail(email, password)
+          .then((res) => {
+            updateProfile(auth.currentUser, {
+              displayName: name,
+              photoURL: data.data.url,
+            })
+              .then((res) => {
+                post(
+                  "users",
+                  {
+                    name,
+                    email,
+                    password,
+                    image: data.data.url,
+                  },
+                  "createUser"
+                )
+                  .then((d) => {
+                    toast.success("Sign up Successful", {
+                      position: "top-right",
+                    });
+                    navigate("/");
+                  })
+                  .catch((err) => console.error(err));
+              })
+              .catch((err) => console.error(err));
+          })
+          .catch((err) => console.error(err));
+      })
+      .catch((error) => {
+        console.error("Error uploading image:", error);
+      });
+  };
+
+  //google login
+  const handleGoogleLogin = () => {
+    console.log("sd");
+    loginWithGoogle()
+      .then((res) => {
+        const { displayName, email, photoURL } = res.user;
+
+        get(`users/${email}`, "getUser").then((data) => {
+          console.log(data);
+          if (data) {
+            navigate("/");
+          } else {
+            post(
+              "users",
+              {
+                name: displayName,
+                email: email,
+                image: photoURL,
+              },
+              "createUser"
+            )
+              .then((res) => res.json())
+              .then((data) => {
+                console.log(data);
+                navigate("/");
+              })
+              .catch((err) => console.error(err));
+          }
+        });
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="flex items-center justify-center h-screen">
-    <div className="w-1/4 p-10 flex items-center justify-center flex-col rounded-md shadow-2xl"
-    style={{
-      background: 'conic-gradient(from 270deg at 0% 0%, rgba(255, 255, 255, .5) 50%, rgba(33, 53, 85, 1) 100%)'
-    }}
-    >
-
-      <div>
-        <img src={logo1} alt="" className="w-32" />
-      </div>
-      <div className="text-4xl mt-4 text-primary font-bold" >Sign Up</div>
-      <form action="" className="w-full flex items-center gap-5 mt-10 flex-col" >
-        <div className="form-control relative  w-full">
-          <input
-            autoComplete="off"
-            id="name"
-            name="name"
-            type="name"
-            className="peer placeholder-transparent h-10 w-full   bg-transparent text-secondary focus:outline-none focus:borer-rose-600 border-b-secondary/50 border-b-2"
-            placeholder="name"
-            required
-          />
-          <label
-            htmlFor="name"
-            className="absolute left-0 -top-3.5 text-gray-200 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-secondary peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-200 peer-focus:text-sm"
-          >
-            Name
-          </label>
+      <div
+        className="w-1/4 p-10 flex items-center justify-center flex-col rounded-md shadow-2xl"
+        style={{
+          background:
+            "conic-gradient(from 270deg at 0% 0%, rgba(255, 255, 255, .5) 50%, rgba(33, 53, 85, 1) 100%)",
+        }}
+      >
+        <div>
+          <img src={logo1} alt="" className="w-32" />
         </div>
-        <div className="form-control relative   w-full">
-          <input
-            autoComplete="off"
-            id="password"
-            name="password"
-            type="password"
-            className="peer placeholder-transparent h-10 w-full   bg-transparent text-secondary focus:outline-none focus:borer-rose-600 border-b-secondary/50 border-b-2"
-            placeholder="password"
-            required
-          />
-          <label
-            htmlFor="password"
-            className="absolute left-0 -top-3.5 text-gray-200 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-secondary peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-200 peer-focus:text-sm"
-          >
-            Password
-          </label>
-        </div>
+        <div className="text-4xl mt-4 text-primary font-bold">Sign Up</div>
+        <form
+          action=""
+          className="w-full flex items-center gap-5 mt-10 flex-col"
+          onSubmit={handleSubmit}
+        >
+          <div className="form-control relative  w-full">
+            <input
+              autoComplete="off"
+              id="name"
+              name="name"
+              type="name"
+              className="peer placeholder-transparent h-10 w-full   bg-transparent text-secondary focus:outline-none focus:borer-rose-600 border-b-secondary/50 border-b-2"
+              placeholder="name"
+              required
+            />
+            <label
+              htmlFor="name"
+              className="absolute left-0 -top-3.5 text-gray-200 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-secondary peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-200 peer-focus:text-sm"
+            >
+              Name
+            </label>
+          </div>
+          <div className="form-control relative  w-full">
+            <input
+              autoComplete="off"
+              id="email"
+              name="email"
+              type="email"
+              className="peer placeholder-transparent h-10 w-full   bg-transparent text-secondary focus:outline-none focus:borer-rose-600 border-b-secondary/50 border-b-2"
+              placeholder="email"
+              required
+            />
+            <label
+              htmlFor="email"
+              className="absolute left-0 -top-3.5 text-gray-200 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-secondary peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-200 peer-focus:text-sm"
+            >
+              Email
+            </label>
+          </div>
+          <div className="form-control relative   w-full">
+            <input
+              autoComplete="off"
+              id="password"
+              name="password"
+              type="password"
+              className="peer placeholder-transparent h-10 w-full   bg-transparent text-secondary focus:outline-none focus:borer-rose-600 border-b-secondary/50 border-b-2"
+              placeholder="password"
+              required
+            />
+            <label
+              htmlFor="password"
+              className="absolute left-0 -top-3.5 text-gray-200 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-secondary peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-200 peer-focus:text-sm"
+            >
+              Password
+            </label>
+          </div>
 
-        <div className="form-control relative w-full">
+          <div className="form-control relative w-full">
             <label htmlFor="instructorImage" className="text-secondary">
               Image
             </label>
             <input
               accept="image/*"
               type="file"
-              name="instructorImage"
+              name="image"
               className="file-input file-input-ghost w-full file:border-r-2 file:border-r-secondary/50   border-t-0 border-l-0 border-r-0 rounded-none border-b-2 text-secondary text-sm border-b-secondary/50"
-            //  onChange={(e) => {
-            //    setUpLoadedImages(e.target.files[0]);
-            //  }}
             />
           </div>
 
-        <button className="SVButton-2 py-2 px-3 mt-3 w-full text-primary" >
-          <span>Sign Up</span>
-        </button>
-      </form>
-      <div className="divider divide-white divide-x-2 divide-y-2 text-primary">OR</div>
-      <div>
-          <button className="border p-2 rounded-full border-primary" >
-              <FaGoogle className="text-5xl text-primary"/>
+          <button className="SVButton-2 py-2 px-3 mt-3 w-full text-primary">
+            <span>Sign Up</span>
           </button>
+        </form>
+        <div className="divider divide-white divide-x-2 divide-y-2 text-primary">
+          OR
+        </div>
+        <div>
+          <button onClick={handleGoogleLogin} className="border p-2 rounded-full border-primary">
+            <FaGoogle className="text-5xl text-primary" />
+          </button>
+        </div>
+
+        <div className="text-primary mt-4">
+          Already have an account? <Link to={"/login"}>Login</Link>
+        </div>
       </div>
-
-      <div className="text-primary mt-4" >Already have an account? <Link to={'/login'}>Login</Link></div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default SignUpPage
+export default SignUpPage;
